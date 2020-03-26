d23a11e548d4697f12acaeefd551788d7fb4e9b8
侯天浩 <hth146@163.com>
2020-03-26 09:57:50 +0800

    11

--- src/main/java/com/holt/bean/EasyUIResult.java
+++ src/main/java/com/holt/bean/EasyUIResult.java
@@ -1,6 +1,4 @@
 这是我清
-/
-/
 所有的不同
 所有的不同
 所有的不同
@@ -10,4 +8,24 @@
 所有的不同
 所有的不同
 所有的不同
+
+加了这一行
+import java.util.Date;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+删掉这一行
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 用户名
+    private String userName;
+
+    // 密码
+    private String password;
 
