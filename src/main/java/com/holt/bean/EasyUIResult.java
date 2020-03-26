fd99af85558891fa831682a8715031889eaed1b3
天浩 <33395164+WZFlik@users.noreply.github.com>
2020-03-26 10:54:21 +0800

    Update EasyUIResult.java

--- src/main/java/com/holt/bean/EasyUIResult.java
+++ src/main/java/com/holt/bean/EasyUIResult.java
@@ -3,32 +3,7 @@ public class User {
  
     // 密码
     private String password;
-    //删除了String
-
-    // 姓名
-    private String name;
-  
-    //加三
-
-    // 年龄
-    private Integer age;
-
-    // 性别，1男性，2女性
-    private Integer sex;
-
-    // 出生日期
-    private Date birthday;
-
-    // 创建时间
-    private Date created;
-
-    // 更新时间
-    private Date updated;
-    //本地加二
-
-    public Long getId() {
-        return id;
-    }
+   
   
    
 //哈哈
