42daa0898f8087b54b1344faf6531b1aa71c91cf
侯天浩 <hth146@163.com>
2020-03-26 10:39:32 +0800

    fff

--- src/main/java/com/holt/bean/EasyUIResult.java
+++ src/main/java/com/holt/bean/EasyUIResult.java
@@ -1,125 +1,16 @@
 //没有了import和package
 public class User {
-    remote加一
-
-    //删除toString
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    //本地加1
-    remote加二
-
-    // 用户名
-    private String userName;
-
-    // 密码
-    private String password;
-    //删除了String
-
-    // 姓名
-    private String name;
-  
-    //加三
-
-    // 年龄
-    private Integer age;
-
-    // 性别，1男性，2女性
-    private Integer sex;
-
-    // 出生日期
-    private Date birthday;
-
-    // 创建时间
-    private Date created;
-
-    // 更新时间
-    private Date updated;
-    //本地加二
-
-    public Long getId() {
-        return id;
-    }
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-   //加
-
-    public String getuserName() {
-        return userName;
-    }
-
-    public void setuserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public Integer getSex() {
-        return sex;
-    }
-
-    public void setSex(Integer sex) {
-        this.sex = sex;
-    }
-
-    public Date getBirthday() {
-        return birthday;
-    }
-
-  //本地删除
-
-    public Date getCreated() {
-        return created;
-    }
-
-    //本地**2
-    public void setCreated(Date created) {
-        this.created = created;
-    }
-
-
-    public void setUpdated(Date updated) {
-        this.updated = updated;
-//    }
-    //本地加三
-        //本地***2
-
+   
     @Override
     public String toString() {
-        return "User [id=" + id + ", userName=" + userName + ", password=" + password + ", name=" + name
+        return "User [id=" + id ====s+ ", userName=" + userName + ", password=" + password + ", name=" + name
                 + ", age=" + age + ", sex=" + sex + ", birthday=" + birthday + ", created=" + created
                 + ", updated=" + updated + "]";
     }
-
+    //本地1
+    //本地2
+    //本地3
+    //本地4
+    //本地15
 
 }
